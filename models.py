58b6de3f472e3ffac983e44a625962537e3b24be
akcsri <akihiko.kaneko@csri-japan.com>
2025-06-11 08:12:07 +0900

    Add files via upload

--- models.py
+++ models.py
@@ -1,13 +1,14 @@
-from werkzeug.security import generate_password_hash, check_password_hash
 
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
     role = db.Column(db.String(50), nullable=False)
     participants = db.relationship('Participant', backref='user', lazy=True)
 
