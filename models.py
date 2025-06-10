49625ef58c81c32c5f23410f0dfb1bb49a0c98fe
akcsri <akihiko.kaneko@csri-japan.com>
2025-06-10 20:29:41 +0900

    Add files via upload

--- models.py
+++ models.py
@@ -19,7 +19,8 @@ class Participant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     position = db.Column(db.String(100))
     name = db.Column(db.String(100))
-    status = db.Column(db.String(100))
+    agm_status = db.Column(db.String(100))
+lpac_status = db.Column(db.String(100))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所有者ユーザー
 
 def get_user_by_username(username):
