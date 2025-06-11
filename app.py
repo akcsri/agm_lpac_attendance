99b7d0024d634da69f09ed2ffe826930de3dc95a
akcsri <akihiko.kaneko@csri-japan.com>
2025-06-12 07:30:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def download_csv():
     participants = Participant.query.all()
     csv_data = "name,email,position,questions,agm_status,lpac_status\n"
     for p in participants:
-        csv_data += f"{p.name},{p.email},{p.position},{p.questions},{p.agm_status},{p.lpac_status}\n"
+        csv_data += f"{p.position},{p.name},{p.agm_status},{p.lpac_status},{p.email},{p.questions}\n"
     # UTF-8 with BOM
     bom = '\ufeff'
     return Response(
