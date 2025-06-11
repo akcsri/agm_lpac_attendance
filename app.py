282e418172b1ca0159861f448653b095bfc2fbe6
akcsri <akihiko.kaneko@csri-japan.com>
2025-06-11 23:09:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, redirect, url_for, render_template
-from flask_login import LoginManager, login_user
+from flask import Flask, request, redirect, url_for, render_template, Response
+from flask_login import LoginManager, login_user, logout_user
 from models import db, User
 
 app = Flask(__name__)
@@ -24,7 +24,7 @@ def login():
         user = User.query.filter_by(username=username).first()
         if user and user.check_password(password):
             login_user(user)
-            if user.role == 'user1':
+           .role == 'user1':
                 return redirect(url_for('user1_dashboard'))
             elif user.role == 'user2':
                 return redirect(url_for('user2_dashboard'))
@@ -35,6 +35,9 @@ def login():
         else:
             return 'Invalid credentials', 401
     return render_template('login.html')
+
+@app.route('/logout')
+def redirect(url_for('login'))
 
 @app.route('/user1_dashboard')
 def user1_dashboard():
@@ -47,6 +50,15 @@ def user2_dashboard():
 @app.route('/admin_dashboard')
 def admin_dashboard():
     return render_template('admin_dashboard.html')
+
+@app.route('/download_csv')
+def download_csv():
+    csv_data = "name,role\nAlice,user1\nBob,user2"
+    return Response(
+        csv_data,
+        mimetype="text/csv",
+        headers={"Content-disposition": "attachment; filename=users.csv"}
+    )
 
 if __name__ == '__main__':
     with app.app_context():
