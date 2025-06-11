a8f95a5bb7f4485a2b93ef8339c02621944516a4
akcsri <akihiko.kaneko@csri-japan.com>
2025-06-11 15:23:11 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-
 from flask import Flask, request, redirect, url_for, render_template
 from flask_login import LoginManager, login_user
-from models import User
+from models import db, User
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -28,4 +37,6 @@ def login():
     return render_template('login.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
